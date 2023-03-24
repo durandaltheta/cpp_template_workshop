34416b6530bad15cd201e754d0f3b5f84bcc851a
dennbla <dennbla@elektrobit.com>
2023-03-24 12:16:27 -0400

    first commit of basic library and ut structure. Added submodule googletest

---
+++ inc/detail/template.hpp
@@ -0,0 +1,17 @@
+#ifndef CPP_TEMPLATE_WORKSHOP_DETAIL_TEMPLATE
+#define CPP_TEMPLATE_WORKSHOP_DETAIL_TEMPLATE 
+
+// cpp stl
+#include <type_traits>
+
+/**
+ * This where code goes which should *not* be directly included by a user, but
+ * is needed by other code included by a user.
+ */
+
+namespace ctw { // cpp template workshop
+namespace detail {
+}
+}
+
+#endif
---
+++ inc/template.hpp
@@ -0,0 +1,21 @@
+#ifndef CPP_TEMPLATE_WORKSHOP_TEMPLATE
+#define CPP_TEMPLATE_WORKSHOP_TEMPLATE 
+
+// cpp stl 
+#include <type_traits>
+#include <vector>
+#include <functional>
+#include <iterator>
+
+// local
+#include "detail/template.hpp"
+
+/**
+ * This is where code goes which can be included by a user, like this project's
+ * unit tests. It defines the public API of our template library.
+ */
+
+namespace ctw { // cpp template workshop
+}
+
+#endif

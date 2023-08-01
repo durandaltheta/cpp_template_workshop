378cd2060192fe8edf7ab3735eab6a56a0c4b4a8
durandaltheta <blaynethomasdennis@gmail.com>
2023-08-01 20:32:57 +0000

    Deploying to gh-pages from @ durandaltheta/cpp_template_workshop@bb67e5044b7408ebfe2c0f819bebb1d8e25a6f0f 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -4,7 +4,7 @@ var searchData=
   ['sfinae_20',['SFINAE',['../structsca_1_1detail_1_1has__size__struct_1_1_s_f_i_n_a_e.html',1,'sca::detail::has_size_struct']]],
   ['sfinae_20challenge_20section_20_2d_20method_20detection_21',['SFINAE CHALLENGE SECTION - method detection',['../md_sfinae_challenge_section.html',1,'']]],
   ['size_22',['size',['../classsca_1_1slice__of.html#a1fd9b1a08a346b028c13e3bfcaf34259',1,'sca::slice_of::size()'],['../classsca_1_1const__slice__of.html#ae9bb7e7b3da98fedbc9c8667ceeedb36',1,'sca::const_slice_of::size()'],['../namespacesca.html#a30d7b247d1fcd4364bbc20018617f9a6',1,'sca::size(C &amp;&amp;c)']]],
-  ['slice_23',['slice',['../namespacesca.html#a22ebd5fb06cb77ea976ccd58cf828b3b',1,'sca']]],
+  ['slice_23',['slice',['../namespacesca.html#a22ebd5fb06cb77ea976ccd58cf828b3b',1,'sca::slice(C &amp;&amp;c, size_t idx, size_t len)'],['../namespacesca.html#a5d417b2153711672fa03fda0efdfa0b8',1,'sca::slice(const C &amp;c, size_t idx, size_t len)'],['../namespacesca.html#a7b5f19dad022bedb6a8ef77f7132e19b',1,'sca::slice(C &amp;c, size_t idx, size_t len)']]],
   ['slice_5fof_24',['slice_of',['../classsca_1_1slice__of.html',1,'sca']]],
   ['some_25',['some',['../namespacesca.html#addcfb883c20eadaed8da521efdac6d34',1,'sca']]],
   ['sort_26',['sort',['../namespacesca.html#ac51d8227d350b7ace5217c3b4562b869',1,'sca']]],

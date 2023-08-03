57468d73f8c29fe538fb4992cd7b63443e66c469
durandaltheta <blaynethomasdennis@gmail.com>
2023-08-03 00:41:10 +0000

    Deploying to gh-pages from @ durandaltheta/cpp_template_workshop@4a8ce65f530b2490b1091cc51b8405e7ba779ef6 🚀

--- search/pages_5.js
+++ search/pages_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['type_20erasure_61',['Type Erasure',['../md_lesson_3_type_erasure.html',1,'']]],
-  ['type_20erasure_20using_20virtual_20inheritance_62',['type erasure using virtual inheritance',['../md_virtual_inheritance.html',1,'']]]
+  ['type_20erasure_20using_20virtual_20inheritance_62',['type erasure using virtual inheritance',['../md_type_erasure_and_virtual_inheritance.html',1,'']]]
 ];

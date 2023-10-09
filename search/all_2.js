704e413f23c2430ab17d98bc7eaaffef156decec
durandaltheta <blaynethomasdennis@gmail.com>
2023-10-09 17:36:33 +0000

    Deploying to gh-pages from @ durandaltheta/cpp_template_workshop@abfd11ccb5e17bf408fc4364d8710b4a534fa69e 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['c_2b_2b_20template_20workshop_4',['C++ template workshop',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['c_2b_2b_20template_20and_20algorithm_20workshop_4',['C++ template and algorithm workshop',['../md__r_e_a_d_m_e.html',1,'']]],
   ['callables_5',['Callables',['../md_lesson_5_callables.html',1,'']]],
   ['const_5fslice_5fof_6',['const_slice_of',['../classsca_1_1const__slice__of.html',1,'sca']]]
 ];
--- search/pages_1.js
+++ search/pages_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['c_2b_2b_20template_20workshop_55',['C++ template workshop',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['c_2b_2b_20template_20and_20algorithm_20workshop_55',['C++ template and algorithm workshop',['../md__r_e_a_d_m_e.html',1,'']]],
   ['callables_56',['Callables',['../md_lesson_5_callables.html',1,'']]]
 ];
